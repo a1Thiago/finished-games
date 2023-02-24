78bc6e49ee9a430c421ee9aecc7c02d013492a03
a1Thiago~ <a1thiago@hotmail.com>
2023-02-24 17:35:45 -0300

    mysql working

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 
 dotenv.config();
@@ -8,10 +8,30 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'finishedGames',
+    password: 'finished-games123',
+    database: 'data'
+})
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
+
+app.get('/games', (req: Request, res: Response) => {
+
+    const queryGames = 'SELECT * FROM data.games;'
+
+    db.query(queryGames, (err: {}, data: []) => {
+        if (err) {
+            return res.json(err)
+        } else {
+            return res.json(data)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
