0bca1af71fc43bd3ff7376364cdf0f96a1a504b9
a1Thiago~ <a1thiago@hotmail.com>
2023-03-13 19:24:52 -0300

    Nav

--- client/src/components/GameCard.stories.tsx
+++ client/src/components/GameCard.stories.tsx
@@ -17,3 +17,4 @@ export const Default = Template.bind({});
 
 Default.args = {
 }
+
--- client/src/components/NavBar.stories.tsx
+++ client/src/components/NavBar.stories.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import GameCard from './GameCard';
+import NavBar from './NavBar';
+
+import { BrowserRouter } from 'react-router-dom';
+
 
 export default {
-  title: 'components/GameCard',
-  component: GameCard,
+  title: 'components/NavBar',
+  component: NavBar,
   argTypes: {
     // backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof GameCard>;
+} as ComponentMeta<typeof NavBar>;
 
-const Template: ComponentStory<typeof GameCard> = (args) => <GameCard {...args} />;
+const Template: ComponentStory<typeof NavBar> = (args) => <BrowserRouter><NavBar /></BrowserRouter>;
+
+
+
+// const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 
 export const Default = Template.bind({});
 
