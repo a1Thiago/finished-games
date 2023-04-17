bc97614ea564f66ac67b289bc41917cbb3c7359d
a1Thiago~ <a1thiago@hotmail.com>
2023-04-17 19:20:40 -0300

    mobile bar

--- client/src/components/GamesNav/GamesNav.tsx
+++ client/src/components/GamesNav/GamesNav.tsx
@@ -2,20 +2,31 @@ import { useNavigate } from "react-router"
 import { sortArray } from '@utils/sortGames'
 import { Button } from "@ui/Button"
 import { ListBox } from "@ui/ListBox"
+import { Funnel } from "@phosphor-icons/react"
 
 type GamesNavProps = {
   selectedOption: React.Dispatch<React.SetStateAction<{}>>
 }
+
 
 export default function GamesNav({ selectedOption }: GamesNavProps) {
 
   const navigate = useNavigate()
 
   return (
-    <div className="flex bg-blue-200 p-4 my-4 justify-between">
+    <div className="grid grid-cols-2 gap-4 bg-blue-200 p-4 my-4">
       <Button label="Add Game" style="primary" onClick={() => navigate('/games/add')} />
-      <div className="h-10 grid justify-center">
-        <ListBox optionsArray={sortArray} selectedOption={selectedOption} />
+
+      {/* +768px */}
+      <div className="h-10 grid justify-end tablet:hidden">
+        <ListBox className="bg-white " optionsArray={sortArray} selectedOption={selectedOption} />
+      </div>
+
+      {/* -768px */}
+      <div className="h-10 grid justify-end md:hidden">
+        <ListBox className="bg-white justify-self-end w-16" optionsArray={sortArray} selectedOption={selectedOption}
+          menuIcon={<Funnel size={20} color="black" weight="fill" />}
+        />
       </div>
     </div>
   )
