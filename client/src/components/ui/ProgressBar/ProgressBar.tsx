e6e00e6a1899cf3b086e254276b85c167a0b4f8f
a1Thiago~ <a1thiago@hotmail.com>
2023-04-19 00:30:25 -0300

    progressBar

--- client/src/components/ui/ProgressBar/ProgressBar.tsx
+++ client/src/components/ui/ProgressBar/ProgressBar.tsx
@@ -10,17 +10,22 @@ export default function ProgressBar(
 
   const [progress, setProgress] = useState(0)
 
+  const [transition, setTransition] = useState(0)
+
   setTimeout(() => {
-    if (progress >= 100) {
-      return setProgress(progress => progress = 0)
-    } else return setProgress(progress => progress + 20)
-  }, 1000)
+    while (progress < 99) {
+      setTransition(transition => transition = 1000)
+      return setProgress(progress => progress + 50)
+    }
+    setTransition(transition => transition = 0)
+    return setProgress(progress => progress = 0)
+  }, 1500)
 
   return (
     <div className='h-1 w-full bg-blue-100 '>
       <div
         style={{ width: `${progressPercentage ?? progress}%` }}
-        className={`transition-all duration-1000 h-full max-w-full ${progress < 30 ? 'bg-blue-300' : progress < 70 ? 'bg-blue-450' : 'bg-blue-700'}`}>
+        className={`transition-all duration-${transition} h-full max-w-full ${progress < 30 ? 'bg-blue-300' : progress < 70 ? 'bg-blue-450' : 'bg-blue-700'}`}>
       </div>
     </div>
   );
